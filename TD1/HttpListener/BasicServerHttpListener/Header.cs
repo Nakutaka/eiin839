e89cfe9892a454787c72024493508ee2fef7b5e4
Nakutaka <alexandre.col@etu.univ-cotedazur.fr>
2021-02-10 15:44:03 +0100

    TD2 Q1

---
+++ TD1/HttpListener/BasicServerHttpListener/Header.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BasicServerHTTPlistener
+{
+    class Header
+    {
+        System.Collections.Specialized.NameValueCollection receivedHeaders;
+        public Header(System.Collections.Specialized.NameValueCollection headers) {
+            receivedHeaders = headers;
+        }
+
+        public void printAllHeaders()
+        {
+            Console.WriteLine("\n\n****** All Received Headers ******\n"+receivedHeaders.ToString());
+        }
+
+        public void printSingleHeader(System.Net.HttpRequestHeader header)
+        {
+            Console.WriteLine("\n\n****** Simple Header: "+header.ToString()+" ******\n"+receivedHeaders.Get(header.ToString()));
+        }
+    }
+}
